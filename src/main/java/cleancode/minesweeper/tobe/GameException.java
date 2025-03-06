7ff5e8395fd3b18c6aca907d4bc39fd193db6e71
KMK_HM <kyun9151@gmail.com>
2025-03-06 16:49:35 +0900

    refactor: SRP 원칙에 의한 관심사 분리

--- src/main/java/cleancode/minesweeper/tobe/GameException.java
+++ src/main/java/cleancode/minesweeper/tobe/GameException.java
@@ -4,9 +4,9 @@ package cleancode.minesweeper.tobe;
  * 의도한 예외
  */
 
-public class AppException extends RuntimeException {
+public class GameException extends RuntimeException {
 
-    public AppException(String message) {
+    public GameException(String message) {
         super(message);
     }
 }
