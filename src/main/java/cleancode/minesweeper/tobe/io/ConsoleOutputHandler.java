9d683229d32bb479ce31af57018364465ddb1fad
KMK_HM <kyun9151@gmail.com>
2025-03-06 18:44:41 +0900

    refactor: ocp 형식으로 리팩토링

--- src/main/java/cleancode/minesweeper/tobe/io/ConsoleOutputHandler.java
+++ src/main/java/cleancode/minesweeper/tobe/io/ConsoleOutputHandler.java
@@ -3,6 +3,9 @@ package cleancode.minesweeper.tobe.io;
 import cleancode.minesweeper.tobe.Cell;
 import cleancode.minesweeper.tobe.GameBoard;
 import cleancode.minesweeper.tobe.GameException;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.IntStream;
 
 public class ConsoleOutputHandler {
 
@@ -13,10 +16,13 @@ public class ConsoleOutputHandler {
     }
 
     public void showBoard(GameBoard board) {
-        System.out.println("   a b c d e f g h i j");
+
+        String joiningAlphabets = generateColAlphabets(board);
+
+        System.out.println("    " + joiningAlphabets);
 
         for (int row = 0; row < board.getRowSize(); row++) {
-            System.out.printf("%d  ", row + 1);
+            System.out.printf("%2d  ", row + 1);
             for (int col = 0; col < board.getColSize(); col++) {
                 System.out.print(board.getSign(row, col) + " ");
             }
@@ -24,6 +30,15 @@ public class ConsoleOutputHandler {
         }
 
         System.out.println();
+    }
+
+    private static String generateColAlphabets(GameBoard board) {
+        List<String> alphabets = IntStream.range(0, board.getColSize())
+                .mapToObj(index -> (char) ('a' + index))
+                .map(Objects::toString)
+                .toList();
+
+        return String.join(" ", alphabets);
     }
 
     public void printGameWinningComment() {
